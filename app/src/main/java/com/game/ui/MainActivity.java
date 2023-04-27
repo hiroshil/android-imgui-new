0313639213f2a1078a6b238eda9383b3ad8e855d
mmc <alexmmc@qq.com>
2023-04-28 00:05:11 +0800

    eeee

--- app/src/main/java/com/game/ui/MainActivity.java
+++ app/src/main/java/com/game/ui/MainActivity.java
@@ -20,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     /*---------------否则输入文本就会闪退---------------*/
     /*----------------------------------------------*/
 
+    ///如果是显示在app的竖屏界面，记得自己给surface设置屏幕宽高，surfaceView自己测算的是屏幕宽高，
+    // 触摸测算的是减去了状态栏标题栏的屏幕宽高，这会导致触摸偏移
+    //当然，如果你是逆向嵌入横屏游戏,那就什么都不用管
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
